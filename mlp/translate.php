d6fcdec2483669dd74164a7fc8f0e5bb2d0fb80d
Justin Sainton <justinsainton@gmail.com>
2018-02-25 16:34:42 -0800

    Translate Tailor layout models, and avoid translating shortcodes

--- mlp/translate.php
+++ mlp/translate.php
@@ -10,7 +10,81 @@ class Translate {
 	}
 
 	public function setup() {
-		add_filter( 'mlp_process_post_data_for_remote_site', array( $this, 'translate_content_on_copy' ), 100, 4 );
+		add_filter( 'mlp_process_post_data_for_remote_site', [ $this, 'translate_content_on_copy' ]    , 100, 4 );
+		add_filter( 'mlp_pre_insert_post_meta'             , [ $this, 'translate_tailor_layout_model' ], 12 , 2 );
+		add_filter( 'mlp_pre_save_post_meta'               , [ $this, 'add_tailor_layout_to_meta' ], 10, 2 );
+	}
+
+	public function add_tailor_layout_to_meta( $meta, $context ) {
+
+		if ( ! isset( $meta['_tailor_layout'] ) ) {
+			$meta['_tailor_layout'] = get_post_meta( $context['real_post_id'], '_tailor_layout', true );
+		}
+
+		return $meta;
+	}
+
+	/**
+	 * When a new post is created, this is hooked in to the pre_filter for inserting remote post meta.
+	 *
+	 * We use this method on this hook to ensure we're intelligently translating only the relevant
+	 * parts of the tailor_layout
+	 *
+	 * @param  [type] $meta    [description]
+	 * @param  [type] $context [description]
+	 * @return [type]          [description]
+	 */
+	public function translate_tailor_layout_model( $meta, $context ) {
+
+		error_log( var_export( $meta, 1 ) );
+
+		// Element Tag => Translatable attributes (keys of `atts` key)
+		$translatable_elements = apply_filters( 'mlp_translatable_tailor_elements', array(
+			'tailor_custom_header'   => array( 'content' ),
+			'tailor_content'         => array( 'content' ),
+			'tailor_custom_top_hero' => array( 'tagline', 'text' ),
+			'tailor_custom_header'   => array( 'content' ),
+			'tailor_button'          => array( 'content' ),
+			'tailor_toggle'          => array( 'title' ),
+		), $meta, $context );
+
+		// Return early if Tailor Layout isn't set.
+		if ( ! isset( $meta['_tailor_layout'] ) ) {
+			return $meta;
+		}
+
+		// If we have empty elements due to the filter, return early.
+		if ( empty( $translatable_elements ) ) {
+			return $meta;
+		}
+
+		$layout = maybe_unserialize( $meta['_tailor_layout'] );
+
+		if ( empty( $layout ) ) {
+			return $meta;
+		}
+
+		$tags          = array_keys( $translatable_elements );
+		$site_language = mlp_get_blog_language( $context['target_blog_id'] );
+
+		foreach ( $layout as $index => $element ) {
+
+			if ( ! in_array( $element['tag'], $tags, true ) ) {
+				continue;
+			}
+
+			$tag = $element['tag'];
+
+			foreach ( $translatable_elements[ $tag ] as $attribute ) {
+				$layout[ $index ]['atts'][ $attribute ] = $this->translate_content( $layout[ $index ]['atts'][ $attribute ], $site_language );
+			}
+
+		}
+
+		$meta['_tailor_layout'] = $layout;
+
+		return $meta;
+
 	}
 
 	/**
@@ -36,7 +110,7 @@ class Translate {
 		$site_language = mlp_get_blog_language( $data['siteID'] );
 
 		$title         = $this->translate( $data['title']  , $site_language );
-		$content       = $this->translate( $data['content'], $site_language );
+		$content       = $this->translate_content( $data['content'], $site_language );
 		$excerpt       = $this->translate( $data['excerpt'], $site_language );
 
 		$data['tinyMCEContent'] = $content;
@@ -72,6 +146,36 @@ class Translate {
 			return $response->data->translations[0]->translatedText;
 		}
 
+	}
+
+	/**
+	 * Handles translating content that may have shortcodes that should NOT be translated.
+	 *
+	 * @param  [type] $text [description]
+	 * @param  [type] $to   [description]
+	 * @return [type]       [description]
+	 */
+	public function translate_content( $text, $to ) {
+
+		$pattern = get_shortcode_regex();
+
+		preg_match_all( "/$pattern/", $text, $matches );
+
+		// If there are no shortcodes, just translate the content.
+		if ( empty( $matches[0] ) ) {
+			return $this->translate( $text, $to );
+		}
+
+		$placeholders = array_fill_keys( $matches[0], '%s' );
+
+		// Replace all the shortcodes in the content with placeholders
+		$content      = strtr( $text, $placeholders );
+
+		// Translate place-held content
+		$translation  = $this->translate( $content, $to );
+
+		// Return the translated content with shortcodes swapped back in.
+		return vsprintf( $translation, $matches[0] );
 	}
 
 }
